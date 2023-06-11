0522519a33ebd6a71a3092a135d470e3287ce50f
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-11 12:39:10 +0800

    Complete 05_sumAll Assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x < 0 || typeof y != "number") return "ERROR";
+    if (x > y) return sumAll(y, x);
+    return (y * (y + 1) - x * (x - 1)) / 2;
 };
 
 // Do not edit below this line
