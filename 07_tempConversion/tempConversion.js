9165bd94b8ad7061215fcdd98ebe7bcff281abb3
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-11 12:43:50 +0800

    Complete 07_tempConversion Assignment

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  return Math.round((x - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  return Math.round((x * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
