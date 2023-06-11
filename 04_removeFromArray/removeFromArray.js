dffd7db03a3b1b652abf1625a8183bc4baeb6900
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-11 12:33:51 +0800

    Complete 04_removeFromArray Assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let flags = [], n = arr.length, res = [];
+    for (let i = 0; i < n; i++) flags.push(false);
+    for (t of args) {
+        for (let i = 0; i < n; i++) {
+            if (arr[i] == t && typeof arr[i] == typeof t) flags[i] = true;
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        if (!flags[i]) res.push(arr[i]);
+    }
+    return res;
 };
 
 // Do not edit below this line
