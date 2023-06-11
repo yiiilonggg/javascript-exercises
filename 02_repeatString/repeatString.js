ef046c713f7e2a545b2b3d87dbe8a131da7d7dba
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-11 10:27:17 +0800

    Complete 02_repeatString Assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    if (n < 0) return "ERROR";
+    let res = "";
+    for (let i = 0; i < n; i++) res += s;
+    return res;
 };
 
 // Do not edit below this line
