99044510f5bb8f25192a42b64182c0b5c818d436
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-15 23:46:39 +0800

    Complete 11_getTheTitles assignment

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let res = [];
+    for (x of arr) res.push(x["title"]);
+    return res;
 };
 
 // Do not edit below this line
