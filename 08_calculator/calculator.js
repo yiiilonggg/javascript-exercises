5d716d862a87119f85c00a2adae1b646ffc56f54
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-15 23:24:17 +0800

    Complete 08_calculator assignment

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;``
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let res = 0;
+  for (x of arr) res += x;
+  return res;``
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let res = 1;
+  for (x of args) res *= x;
+  return res;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	let res = 1;
+  for (let i = 0; i < y; i++) res *= x;
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let res = 1;
+  while (x > 0) {
+    res *= x;
+    x--;
+  }
+  return res;
 };
 
 // Do not edit below this line
