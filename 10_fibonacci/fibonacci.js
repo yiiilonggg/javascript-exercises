c61ed2794c9bd77beafc2edc5ae2693a1b6c9136
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-15 23:45:15 +0800

    Complete 10_fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (typeof x == "string") x = Number(x);
+    if (x < 0) return "OOPS";
+    if (x <= 2) return 1;
+    let prevPrev = 1, prev = 1, curr = 2;
+    for (let i = 2; i < x; i++) {
+        curr = prevPrev + prev;
+        prevPrev = prev;
+        prev = curr;
+    }
+    return prev;
 };
 
 // Do not edit below this line
