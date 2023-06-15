4bb8df9425f464e082872a47a64cdedaf8162890
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-16 00:06:44 +0800

    Complete 12_findTheOldest assignment

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((a, b) => {
+        const x = getAge(a["yearOfBirth"], a["yearOfDeath"]);
+        const y = getAge(b["yearOfBirth"], b["yearOfDeath"]);
+        return (x < y) ? b : a;
+    })
 };
+
+function getAge(birth, death) {
+    if (typeof death == "undefined") death = 2023;
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
