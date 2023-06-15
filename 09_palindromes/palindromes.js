aad0a6c09b9aea09a6eecd1824eb4919833e56df
yiiilonggg <120325727+yiiilonggg@users.noreply.github.com>
2023-06-15 23:36:47 +0800

    Complete 09_palindromes assignment

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+function shift(s, x, dir) {
+    let l = s.length;
+    while (x >= 0 && x < l) {
+        let code = s.charCodeAt(x);
+        if (!(code > 47 && code < 58) &&
+            !(code > 64 && code < 91) &&
+            !(code > 96 && code < 123)) {
+                x += dir;
+            } else {
+                break;
+            }
+    }  
+    return x;
+}
 
+const palindromes = function (s) {
+    s = s.toUpperCase();
+    let left = shift(s, 0, 1), right = shift(s, s.length - 1, -1);
+    while (left < right) {
+        if (s.charAt(left) != s.charAt(right)) return false;
+        left = shift(s, left + 1, 1);
+        right = shift(s, right - 1, -1);
+    }
+    return true;
 };
 
 // Do not edit below this line
